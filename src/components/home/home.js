dabaa9348c527aa6c4155a5a32089da40c8b163b
tiansheng.wang <tiansheng.wang@ucarinc.com>
2019-08-21 18:08:50 +0800

    修改axios请求设置

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,11 +3,25 @@ import axios from 'axios';
 import index from '../../config/index';
 import './home.scss';
 
+axios.defaults.withCredentials = true;
+axios.interceptors.response.use(
+    response => {
+        if (response.data.msg === 401) {
+            window.location.href = "http://www.baidu.com";
+        }
+        return response;
+    },
+    error => {
+        console.log("请求错误，请重试");
+        return Promise.reject(error);
+    }
+);
+
 class home extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {username: '', myText: ''};
+        this.state = {username: '', myText: '', logout: ''};
     }
 
     componentDidMount() {
@@ -37,18 +51,29 @@ class home extends Component {
         });
     }
 
+    handleClick2 = (e) => {
+        axios.post("http://127.0.0.1:5000/back/account/logout", {password: this.state.username}).then((res) => {
+            console.log(res);
+            this.setState({logout: res.data.msg});
+        }).catch((e) => {
+            console.log(e);
+        });
+    }
+
     render() {
         return (
             <div>首页 3000<br/>
                 <input value={this.state.myText} onChange={e => this.setState({myText: e.target.value})}
                        placeholder='get' autoFocus='true' required/>&nbsp;&nbsp;
-                <button onClick={this.handleClick}>get 请求</button>
+                <button onClick={this.handleClick}>登录</button>
                 <br/>
                 <input value={this.state.username} onChange={e => this.setState({username: e.target.value})}
                        placeholder='post' autoFocus='true' required/>&nbsp;&nbsp;
                 <button onClick={this.handleClick1}>post请求</button>
+                <button onClick={this.handleClick2}>退出</button>
                 <div>{this.state.myText}</div>
                 <div>{this.state.username}</div>
+                <div>{this.state.logout}</div>
             </div>
         )
     }
